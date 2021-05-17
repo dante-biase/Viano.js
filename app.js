2b9b87faeb7b7c06abce896c8aaa65e300f72ccb
dante-biase <dante.biase@outlook.com>
2021-05-17 04:22:15 -0400

    heroku route fix attempt: 4

--- app.js
+++ app.js
@@ -51,25 +51,18 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
-if (process.env.NODE_ENV === "production") {
-  // Express will serve up production assets
-  // like our main.js file, or main.css file
-  app.use(express.static("client/build"));
-
-}
-else {
-  // error handler
-  app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-  });
-}
+// const port = process.env.PORT || 3000;
+// app.listen(port);
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,9 @@ const express = require('express');
 const Song = require('../models/Song.js');
 const router = express.Router();
 
-// /* GET home page. */
-// router.get('/', (req, res, next) => {
-//   res.render('index', { title: 'pjs' });
-// });
-
-const path = require("path");
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+/* GET home page. */
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'pjs' });
 });
 
 module.exports = router;
